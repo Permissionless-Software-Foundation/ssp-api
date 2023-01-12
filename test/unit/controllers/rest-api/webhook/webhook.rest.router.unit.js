8dc1867cb95ebb7c422f689d183ea30d3b3fa307
Chris Troutner <chris.troutner@gmail.com>
2023-01-12 15:28:12 -0800

    feat(webhook): POST /webhook/store REST API endpoint

--- test/unit/controllers/rest-api/webhook/webhook.rest.router.unit.js
+++ test/unit/controllers/rest-api/webhook/webhook.rest.router.unit.js
@@ -1,33 +1,25 @@
 /*
-  Unit tests for the REST API handler for the /users endpoints.
+  Unit tests for the REST API handler for the /webhook endpoints.
 */
 
 // Public npm libraries
 import { assert } from 'chai'
-
 import sinon from 'sinon'
 
-// Local support libraries
+// Local libraries
 import adapters from '../../../mocks/adapters/index.js'
-
 import UseCasesMock from '../../../mocks/use-cases/index.js'
-
-// const app = require('../../../mocks/app-mock')
-
-import ContactRouter from '../../../../../src/controllers/rest-api/contact/index.js'
+import WebhookRouter from '../../../../../src/controllers/rest-api/webhook/index.js'
 
 let uut
 let sandbox
-// let ctx
 
-// const mockContext = require('../../../../unit/mocks/ctx-mock').context
-
-describe('#Contact-REST-Router', () => {
+describe('#Webhook-REST-Router', () => {
   // const testUser = {}
 
   beforeEach(() => {
     const useCases = new UseCasesMock()
-    uut = new ContactRouter({ adapters, useCases })
+    uut = new WebhookRouter({ adapters, useCases })
 
     sandbox = sinon.createSandbox()
 
@@ -40,26 +32,26 @@ describe('#Contact-REST-Router', () => {
   describe('#constructor', () => {
     it('should throw an error if adapters are not passed in', () => {
       try {
-        uut = new ContactRouter()
+        uut = new WebhookRouter()
 
         assert.fail('Unexpected code path')
       } catch (err) {
         assert.include(
           err.message,
-          'Instance of Adapters library required when instantiating Contact REST Controller.'
+          'Instance of Adapters library required when instantiating Webhook REST Controller.'
         )
       }
     })
 
     it('should throw an error if useCases are not passed in', () => {
       try {
-        uut = new ContactRouter({ adapters })
+        uut = new WebhookRouter({ adapters })
 
         assert.fail('Unexpected code path')
       } catch (err) {
         assert.include(
           err.message,
-          'Instance of Use Cases library required when instantiating Contact REST Controller.'
+          'Instance of Use Cases library required when instantiating Webhook REST Controller.'
         )
       }
     })
